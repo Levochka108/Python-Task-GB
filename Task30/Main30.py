5864e22eaef429ed4e6511e577bee48fd63da5bb
dmitrii klochkov <d.klochkov9421@gmail.com>
2023-03-07 21:53:44 +0300

    First commit Task30

--- Task30/Main30.py
+++ Task30/Main30.py
@@ -1,24 +1,37 @@
 from tkinter import *
 from tkinter import ttk
-
-root = Tk()
-
-root.title("Task28")
-root.geometry("250x200")
-
-entery = ttk.Entry()
-entery.pack()
-
-btn = ttk.Button()
-btn.pack(anchor = SE, padx = 15, pady = 15,expand=True)
+import Message
 
 
+def Main():
+
+    root = Tk()
+
+    root.title("Task28")
+
+    root.geometry("300x200")
+    root["bg"] = "gray26"
+
+    def click():
+        label["text"] = entery.get()
+        print("Команда выполнина!")
+
+    entery = ttk.Entry()
+    entery.pack(anchor=NW, padx=6, pady=6)
+
+    label = ttk.Label()
+    label.pack(anchor=NW, padx=6, pady=6)
+
+    # Кнопка Выход
+    btn = Button(root, bg='#8d8d8d', text="Выход",  fg='white')
+    btn.pack(side=LEFT, anchor=SW, padx=15, pady=15)
+
+    # Кнопка далее
+    btn_Exit = Button(root, bg='#8d8d8d', text="Далее",
+                      fg='white', command=click)
+    btn_Exit.pack(side=RIGHT, anchor=SE, padx=15, pady=15)
+
+    root.mainloop()
 
 
-btn["text"] = "Далее"
-
-btnText = btn["text"]
-
-print(btnText)
-
-root.mainloop()
+Main()
