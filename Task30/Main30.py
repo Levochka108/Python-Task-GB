4272ef88cd879da4b0af8a8e122c678c52d09202
dmitrii klochkov <d.klochkov9421@gmail.com>
2023-03-08 11:25:22 +0300

    add task30 and add interface for task

--- Task30/Main30.py
+++ Task30/Main30.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 import customtkinter as ctk
+import os
 
 
 class Program():
     def Main():
         # Modes: system (default), light, dark
-        ctk.set_appearance_mode("dark")
+        ctk.set_appearance_mode("DARK")
         # Themes: blue (default), dark-blue, green
         ctk.set_default_color_theme("blue")
 
@@ -19,22 +20,33 @@ class Program():
         frame_1.pack(pady=25, padx=10, fill="both", expand=True)
 
         # тесет в нутри фоногвой области в окне
-        text_1 = ctk.CTkTextbox(master=frame_1, width=444, height=100)
+        text_1 = ctk.CTkTextbox(master=frame_1, width=444,
+                                height=100, font=("Times", 14))
         text_1.pack(pady=10, padx=10)
-        text_temp = f"Задача 30: \nЗаполните массив элементами арифметической прогрессии.\
-            \nЕё первый элемент, разность и количество элементов нужно ввестис клавиатуры. Формула для получения n-го члена прогрессии: \
-            an = a1 + (n-1) * d. Каждое число вводится с новой строки."
+        text_temp = f"Задача 30:\n\
+        Заполните массив элементами арифметической прогрессии.\
+        Её первый элемент, разность и количество элементов нужно ввестис клавиатуры.\n\
+        Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d. Каждое число вводится с новой строки."
         text_1.insert("0.0", text_temp)
 
-        label = ctk.CTkLabel(master=frame_1, text="Введите элименты")
+        label = ctk.CTkLabel(
+            master=frame_1, text="Введите элементы", font=("Times", 20))
         label.pack(pady=12, padx=10)
 
-        entry_1 = ctk.CTkEntry(master=frame_1, placeholder_text="Ввод >")
+        # Поля ввода текста
+        entry_1 = ctk.CTkEntry(
+            master=frame_1, placeholder_text="Ввод >", font=("Times", 14))
         entry_1.pack(pady=1, padx=1)
-        entry_2 = ctk.CTkEntry(master=frame_1, placeholder_text="Ввод >")
+        entry_2 = ctk.CTkEntry(
+            master=frame_1, placeholder_text="Ввод >", font=("Times", 14))
         entry_2.pack(pady=2, padx=2)
-        entry_3 = ctk.CTkEntry(master=frame_1, placeholder_text="Ввод >")
+        entry_3 = ctk.CTkEntry(
+            master=frame_1, placeholder_text="Ввод >", font=("Times", 14))
         entry_3.pack(pady=3, padx=3)
+
+        label = ctk.CTkLabel(
+            master=frame_1, text="*Для получения ответа нажать Далее.", font=("Times", 11))
+        label.pack(pady=10, padx=20)
 
         def button_function():
             value1 = 0
@@ -44,7 +56,6 @@ class Program():
             value2 = int(entry_2.get())
             value3 = int(entry_3.get())
             arr = [value1 + (i-1)*value2 for i in range(1, value3 + 1)]
-
             print(f"Done!!")
             print(arr)
 
@@ -54,10 +65,10 @@ class Program():
 
         # Use CTkButton instead of tkinter Button
         button = ctk.CTkButton(
-            master=frame_1, text="Далее", command=button_function)
+            master=frame_1, text="Далее", font=("Times", 14),  command=button_function)
         button.place(relx=0.64, rely=0.94, anchor=tk.SW)
         button_Exit = ctk.CTkButton(
-            master=frame_1, text="Выход", command=button_Exit)
+            master=frame_1, text="Выход", font=("Times", 14), command=button_Exit)
         button_Exit.place(relx=0.2, rely=0.94, anchor=tk.S)
 
         app.mainloop()
--- Task30/Message.py
+++
@@ -1,7 +0,0 @@
-a1 = int(input("Enter the first element: "))
-d = int(input("Enter the common difference: "))
-n = int(input("Enter the number of elements: "))
-
-arr = [a1 + (i-1)*d for i in range(1, n+1)]
-
-print("The array is:", arr)
